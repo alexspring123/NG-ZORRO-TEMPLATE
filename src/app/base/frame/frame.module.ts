c75c2d24af395ba2b66d5c21e4e7a0b94e0f078e
alex <alex>
2017-09-22 12:58:32 +0800

    路由改造为路由模块，支持模块异步加载  #8

--- src/app/base/frame/frame.module.ts
+++ src/app/base/frame/frame.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from "@angular/core";
-import { FrameComponent } from "app/base/frame/frame.component";
-import { NgZorroAntdModule } from "ng-zorro-antd";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { ContentsModules } from "app/config/contents-modules";
+import { NgZorroAntdModule } from "ng-zorro-antd";
+import { FrameComponent } from "app/base/frame/frame.component";
 import { HeadUserComponent } from "app/base/shared/heard/head-user.component";
 import { HeadComponent } from "app/base/shared/heard/head.component";
 import { MenuComponent } from "app/base/shared/menu/menu.component";
 import { LoginServiceImpl } from "app/providers/login.service-impl";
-import { ChangePasswordModule } from "app/base/change-password/change-password.module";
 import { FrameRoutingModule } from "app/base/frame/frame-routing.module";
-import { RoleModule } from "app/contents/role-demo/role.module";
 
 @NgModule({
     imports: [
