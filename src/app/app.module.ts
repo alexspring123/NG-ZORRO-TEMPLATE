f57cd04fc6c789d31a4cebff22d0d5f5a8072d7b
alex <alex>
2017-09-28 14:12:56 +0800

    根模块appModule全局导入Http模块 #11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
+import { HttpModule } from '@angular/http';
+
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,6 +17,7 @@ import { SessionService } from 'app/base/shared/session.service';
   imports: [
     BrowserModule,
     RouterModule,
+    HttpModule,
     BrowserAnimationsModule,
     NgZorroAntdModule.forRoot(),
     AppRoutingModule,
